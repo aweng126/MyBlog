a9f1310641b4e10f7300a2025a883e9d5ec99a29
aweng126 <13075313106@163.com>
2017-04-17 16:57:09 +0800

    成功实现后台admin登录

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,56 +1,24 @@
-class SessionsController < ApplicationController
-  before_action :set_session, only: [:show, :edit, :update, :destroy]
-
-  # GET /sessions
-  # GET /sessions.json
-  def index
-    @sessions = Session.all
-  end
-
-  # GET /sessions/1
-  # GET /sessions/1.json
-  def show
-  end
+class Admin::SessionsController < ApplicationController
+  before_action :set_session, only: [ :destroy]
 
   # GET /sessions/new
   def new
     @session = Session.new
   end
 
-  # GET /sessions/1/edit
-  def edit
-  end
-
-=begin
-  用户登录
-=end
   def create
-    @user = user.authenticate(params[:name], params[:pass])
 
-    p params[:name]
-    p params[:pass]
+    # p params[:name]
+    # p params[:pass]
 
-    if @user
-      session[:user_id] = @user.id
-      flash[:notice] = "Weclome #{@user.login}"
-      redirect_to posts_path
-    else
-      flash[:notice] = "The login or password is not correct."
-      redirect_to new_session_path
+    # 需要进行进一步修改，搜索条件进行调整，通过name和pass一次搜索。
+    @user=User.find_by_name(params[:name])
+    if(@user.pass==params[:pass])
+       redirect_to posts_path
     end
+
   end
 
-  def update
-    respond_to do |format|
-      if @session.update(session_params)
-        format.html { redirect_to @session, notice: 'Session was successfully updated.' }
-        format.json { render :show, status: :ok, location: @session }
-      else
-        format.html { render :edit }
-        format.json { render json: @session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /sessions/1
   # DELETE /sessions/1.json
